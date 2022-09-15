2518b44590cea4c238a041f4fe8329df8b56f3dc
Favour58 <110856204+Favour58@users.noreply.github.com>
2022-09-15 14:37:13 -0700

    Add files via upload

---
+++ wavy.js
@@ -0,0 +1,19 @@
+"use strict"
+let conf =confirm("This Site Allows Cookies")
+// let age =prompt("How old are you?","")
+
+// if(age<=17){
+// 	alert("You're too young to view this content,log out now or you will be denied permanent access")
+// }
+// else{
+// 	alert("You're old enough to view this content....Please PROCEED!!");
+// }
+
+// let prom = prompt("what's you actual age??","")
+// if (prom <=17){
+// 	alert("You are NOT old enough to access this site....please EXIT!!")
+// }
+// else{
+// 	alert("You're eligible to access this site....Welcome!!")
+// }
+
